adbd2cf90106e144e1393ecf4e8ef7f248c35624
Lauren Leslie Ferreira <laurenlesliesilva@gmail.com>
2024-02-21 17:05:00 -0300

    fix: px to rem

--- src/layouts/default/styles.ts
+++ src/layouts/default/styles.ts
@@ -5,9 +5,10 @@ export const Container = styled.div`
   flex-direction: column;
   flex: 1;
   height: 100%;
-  max-width: 1120px;
+  max-width: 70rem;
   width: 100%;
   margin: 0 auto;
+  padding: 0 2rem;
 
   color: ${({ theme }) => theme.colors.neutral[900]};
 `;
